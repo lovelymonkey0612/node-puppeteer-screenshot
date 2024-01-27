48214581c0b7e9895b9bc4fe19753f6c83a21173
Denis Martin <denysmartin4952@gmail.com>
2024-01-27 09:15:16 -0800

    update/screen size

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ async function main() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://oxylabs.io/', {timeout: 0});
+  await page.setViewport({
+    width: 1920,
+    height: 1080,
+  });
   await page.screenshot({'path': 'oxylabs_js.png'})
   await browser.close();
 }
